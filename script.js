1e394e5db5e5ac1b142ab260d570d40a9e465e87
94Kulin <83169695+94Kulin@users.noreply.github.com>
2025-04-30 13:23:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showZoom(imageName) {
   console.log("Hover detected:", imageName); // Add this line
   const zoomedView = document.getElementById('zoomedView');
   const zoomedImg = document.getElementById('zoomedImg');
-  zoomedImg.src = `images/${imageName}.jpeg`;
+  zoomedImg.src = `images/${imageName}.jpg`;
   zoomedView.style.display = 'block';
 }
 
